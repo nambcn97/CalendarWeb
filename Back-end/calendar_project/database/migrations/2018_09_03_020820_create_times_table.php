62c4726efddaf5f3a24e635d2bff5efae7f8d02d
Zul <thoofadhli1987@gmail.com>
2018-09-08 14:40:24 +0800

    database connection
    Local database connection

--- Back-end/calendar_project/database/migrations/2018_09_03_020820_create_times_table.php
+++ Back-end/calendar_project/database/migrations/2018_09_03_020820_create_times_table.php
@@ -16,8 +16,7 @@ class CreateTimesTable extends Migration
         Schema::create('times', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name_of_event');
-            $table->datetime('date_of_event');
-            
+            $table->datetime('date_of_event'); 
             $table->timestamps();
         });
     }
